53c001fb92192e59d2ff61b006e96d0338423cf7
eliudium <77957124+eliudistic@users.noreply.github.com>
2024-10-04 12:25:04 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Tinega.co - All Rights Reserved.</p>
+      <p>Sponsored by [Sponsor Logos Here]</p>
+    </footer>
+  );
+};
+
+export default Footer;
 
